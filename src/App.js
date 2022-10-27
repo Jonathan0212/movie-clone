8d7090a1c020c6c6c21a40fb2c28ea806b4c5be9
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-10-27 13:43:11 -0500

    fetched data from API by using Star Wars title. Working on styling prototypes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import {useEffect} from "react";
+import './App.css';
+// import './search.svg';
+
+// 723298ef
+const API_URL = 'http://www.omdbapi.com?apikey=723298ef'
 
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Star Wars')
+    }, []);
     return (
-        <h1> Welcome React </h1>
+        <div className="app">
+            <h1> Movielandia </h1>
+        </div>
     );
 }
 
