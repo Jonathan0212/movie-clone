c19644b2beceab7be80b155bce7918329ddab33f
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-10-28 07:42:38 -0500

    added movie card component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import './App.css';
 import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
 
 // 723298ef
 const API_URL = 'http://www.omdbapi.com?apikey=723298ef'
 
+
+const movie1 = {
+    "Title": "Star Wars: Episode IV - A New Hope",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
+}
+
 const App = () => {
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-
         console.log(data.Search);
     }
 
     useEffect(() => {
         searchMovies('Star Wars')
     }, []);
+
     return (
         <div className="app">
             <h1> Movielandia </h1>
@@ -26,6 +38,7 @@ const App = () => {
                 value='super'
                 onChange={() => {}}
             />
+            
             <img 
                 src={SearchIcon}
                 alt='search'
@@ -33,12 +46,22 @@ const App = () => {
             />
           </div>
 
-          <div className="container">
+          {
+              movies?.length > 0
+              ? (  
+                <div className="container">
+                    <MovieCard movie1={movie1}/>
+                 </div>
+              ) : (
+                  <div className="empty">
+                    <h2> No Movies Found </h2>  
+                  </div>
+              )
+          }
 
-          </div>
-        </div>
-        
+         
+    </div>
     );
-}
+};
 
 export default App;
